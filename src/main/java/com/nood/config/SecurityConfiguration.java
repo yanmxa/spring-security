fbcc8a1ed4fb5b94847f614b47212e2d9037b19f
Yanmeng <ymeng125@126.com>
2020-11-19 18:07:54 +0800

    auto login without manual create persistent_logins table

--- src/main/java/com/nood/config/SecurityConfiguration.java
+++ src/main/java/com/nood/config/SecurityConfiguration.java
@@ -70,7 +70,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public PersistentTokenRepository persistentTokenRepository() {
         JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
         jdbcTokenRepository.setDataSource(dataSource);
-//        jdbcTokenRepository.setCreateTableOnStartup(true);
+        jdbcTokenRepository.setCreateTableOnStartup(true);
         return jdbcTokenRepository;
     }
 
